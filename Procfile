13f81305a4c5308d4d83130647396843cc268432
skupriienko <skromnitsky@gmail.com>
2020-09-09 14:07:25 +0300

    first commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn main:app
+celery: celery worker -A celery_runner
---
+++ gserver.py
@@ -0,0 +1,6 @@
+from gevent.pywsgi import WSGIServer
+from webapp import create_app
+
+app = create_app('webapp.config.ProdConfig')
+server = WSGIServer(('', 80), app)
+server.serve_forever()
